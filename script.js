e88d7878d5b7b856e6691f951057cfd7751a24d5
IsaAfonsoCollares <isabela_afonso_collares@hotmail.com>
2021-08-22 20:52:36 -0300

    segundo commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const preencherFormulario = (endereco) => {
 }
 const pesquisarCep = async() => {
     const cep = document.getElementById('cep').value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
     const dados = await fetch(url);
     const endereco = await dados.json();
     preencherFormulario (endereco);
